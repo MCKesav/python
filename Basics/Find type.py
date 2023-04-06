d1d29d9b1ff8cf97fdfb78d8d7af6af6ff1f6f7c
MCKesav <129755532+MCKesav@users.noreply.github.com>
2023-04-06 14:33:13 +0530

    Create Find type.py

---
+++ Basics/Find type.py
@@ -0,0 +1,12 @@
+a=45
+b=2.5
+c=(34+45j)
+d=("hi")
+e=True
+
+print("This is a type of -", type(a))
+print("This is a type of -", type(b))
+print("This is a type of -", type(c))
+print("This is a type of -", type(d))
+
+print("This is a type of -", type(e))

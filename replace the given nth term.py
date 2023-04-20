1f15624daa6900ef360f6913ced12c148f000035
MCKesav <129755532+MCKesav@users.noreply.github.com>
2023-04-20 21:36:29 +0530

    Delete replace the given nth term.py

--- replace the given nth term.py
+++
@@ -1,5 +0,0 @@
-input_string = input("Enter the string: ")
-nth_number = int(input("Entere the nth number: "))
-first_string = input_string[:nth_number]
-second_string = input_string[nth_number:]
-print(first_string[0:]+second_string[1:])

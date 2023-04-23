49542fb2fed6bf02cf0290200cbf7593d612aa03
MCKesav <129755532+MCKesav@users.noreply.github.com>
2023-04-24 00:05:50 +0530

    Update Find type.py

--- Basics/Find type.py
+++ Basics/Find type.py
@@ -3,10 +3,8 @@ b=2.5
 c=(34+45j)
 d=("hi")
 e=True
-
 print("This is a type of -", type(a))
 print("This is a type of -", type(b))
 print("This is a type of -", type(c))
 print("This is a type of -", type(d))
-
 print("This is a type of -", type(e))
